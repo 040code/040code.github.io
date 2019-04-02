b1b6e78a18e0b9c07268883214e161e979f46803
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-04-02 21:51:19 +0200

    :sparkles: Feat: Adds clickable authors to posts

--- src/templates/authors.js
+++ src/templates/authors.js
@@ -3,51 +3,37 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 
 import Layout from '../components/layout'
-import Wrapper from '../components/Wrapper'
-import Hero from '../components/Hero'
 import PostsList from '../components/PostsList'
-import Pagination from '../components/Pagination'
+import Wrapper from '../components/Wrapper'
 import SEO from '../components/SEO'
+import Hero from '../components/Hero'
 
-class BlogList extends React.Component {
+class Authors extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const pageTitle = `#${this.props.pageContext.author}`
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
-    const { pageContext } = this.props
 
     return (
-      <Layout location={this.props.location}>
-        <SEO />
-        <Hero title={siteTitle} />
+      <Layout location={this.props.location} title={pageTitle}>
+        <SEO title={pageTitle} />
+        <Hero title={pageTitle} />
 
         <Wrapper>
+          <h1>Posts authored by "{this.props.pageContext.author}"</h1>
           <PostsList posts={posts} />
         </Wrapper>
-
-        <Pagination
-          nbPages={pageContext.nbPages}
-          currentPage={pageContext.currentPage}
-        />
       </Layout>
     )
   }
 }
 
-export default BlogList
+export default Authors
 
 export const pageQuery = graphql`
-  query blogListQuery($skip: Int!, $limit: Int!) {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
+  query PostsByAuthor($author: String!) {
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { type: { ne: "page" } } }
-      limit: $limit
-      skip: $skip
+      filter: { frontmatter: { authors: { eq: $author } } }
     ) {
       edges {
         node {
